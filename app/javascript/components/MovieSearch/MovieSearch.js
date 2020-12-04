03a733571d803ece619024d550f820c41949beea
rilindmehmeti <rilind.h.mehmeti@gmail.com>
2020-12-04 22:33:00 +0100

    Adds search header.

--- app/javascript/components/MovieSearch/MovieSearch.js
+++ app/javascript/components/MovieSearch/MovieSearch.js
@@ -3,10 +3,23 @@ import SearchHeader from "./SearchHeader/SearchHeader";
 import SearchResults from "./SearchResults/SearchResults";
 
 class MovieSearch extends Component {
+  state = {
+    searchTerm: "",
+    movies: []
+  }
+
+  searchTermUpdateHandler = (searchTerm) => {
+    this.setState({searchTerm: searchTerm.target.value})
+  }
+
+  searchClickHandler = () => {
+    alert("You Searched!");
+  }
+
   render() {
     return(
       <>
-        <SearchHeader />
+        <SearchHeader searchTerm={this.state.searchTerm} searchTermUpdated={this.searchTermUpdateHandler} searchClick={this.searchClickHandler}/>
         <SearchResults />
       </>
     )
--- app/javascript/components/MovieSearch/SearchHeader/SearchHeader.js
+++ app/javascript/components/MovieSearch/SearchHeader/SearchHeader.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
-class SearchHeader extends Component {
-  render() {
+const SearchHeader = (props) => {
+    let { searchTerm, searchTermUpdated, searchClick } = props;
+
+    const canSearch = () => {
+      return(searchTerm.length > 3)
+    }
+
     return(
-      <header>
-        Header Goes Here....
+      <header className="search-header">
+        <div className="container">
+          <div className="row">
+            <div className="col-10">
+              <input className="form-control control search-input" value={searchTerm} onChange={searchTermUpdated} placeholder="Search movie..."/>
+            </div>
+            <div className="col-2">
+              <button className="form-control control search-button" disabled={!canSearch()} onClick={searchClick}>Search</button>
+            </div>
+          </div>
+        </div>
       </header>
-    )
-  }
+    );
 }
 
 export default SearchHeader;
